ccf384997e410a84dfee5c2429b1df387ce7d829
Tanmeet Kaur <tankaur4@publicisgroupe.net>
2023-12-15 10:18:23 +0530

    commit

--- Flight/src/main/java/org/example/Booking.java
+++ Flight/src/main/java/org/example/Booking.java
@@ -1,4 +1,13 @@
 package org.example;
 
-public class Booking {
+public interface Booking {
+    public void addBook(Booking booking);
+    public void updateBook(Booking booking);
+    public void removeBook(Booking booking);
+    public void findBookByName(String name);
+    public void findBookByCategory(String category);
+    public void findBookByCost(Float price);
+    public void findBookByCostRange(Float startingPrice, Float endingPrice);
+    public void findCheapestBook();
+    public void findCostliestBook();
 }
--- Flight/src/main/java/org/example/Ticket.java
+++ Flight/src/main/java/org/example/Ticket.java
@@ -1,4 +1,50 @@
 package org.example;
 
 public class Ticket {
+    private String title;
+    private Integer ID;
+    private Float price;
+    private String category;
+
+    public Ticket() {
+    }
+
+    public Ticket(String title, Integer ID, Float price, String category) {
+        this.title = title;
+        this.ID = ID;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
